4974f4a3e423b978488a81043beb1334b05151d0
Francois Carstens <theconstructagency@gmail.com>
2016-11-04 10:00:24 -0500

    update boilerplate

--- src/scripts/views/detailView.js
+++ src/scripts/views/detailView.js
@@ -5,7 +5,25 @@ import Footer from './footer'
 var DetailView = React.createClass({
 	render: function() {
 		return (
+			<div class="body-container">
+				<Header />
+				<section className="">
+					<Image model="this.props.<endpoint_here>" />
+					<Likes />
+					<Comments model="this.props.<endpoint_here>" />
+				</section>
+				<Footer />
+			</div>
+		)
+	}
+})
 
+var Image = React.createClass({
+	render: function() {
+		return (
+			<div className="main-image">
+			
+			</div>
 		)
 	}
 })
