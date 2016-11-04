8dbc06ff32f3700bfe505e5fe25853f3df4ffcd5
Shar'nee Francis <sharneefrancis@Sharnees-MacBook-Pro.local>
2016-11-04 10:13:30 -0500

    controller built

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
+import Header from './view/header.js'
 
 const app = function() {
-
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
+//MODEL
+	var PictureMOdel = Backbone.Collection.extend({
+		url: 'some url',
+		_key: 'some key',
+		//temp parse function if needed. I don't think we will need this but it's here if we do. 
+		parse: function(rawResponse){
+	 			var parsedResponse = rawResponse.results
+	 			return parsedResponse
+	 		}
+		})
+//CONTROLLER
+	var Controller = Backbone.Router.extend({
+		routes:{
+			'home': 'handleGrid',
+			'search/:term': 'handleSearch',
+			'detail/:id': 'handlDetail',
+			'scroll': 'handleScroll',
+			'creator': 'handleCreator',
+			'*default': 'handleDefault'
+		},
+		handleGrid: function(){
+			console.log('handeling Grid')
+		},
+		handleSearch: function(){
+			console.log('handeling Search')
+		},
+		handlDetail: function(){
+			console.log('handeling detail')
+		},
+		handleScroll: function(){
+			console.log('handeling the Scroll')
+		}, 
+		handleCreator: function(){
+			console.log('handeling the creator view')
+		}, 
+		handleDefault: function(){
+			location.hash = 'home'
+			console.log('handeling Default')
+		}, 
+		initialize: function(){
+			Backbone.history.start()
+			console.log('being born now')
 		}
 	})
-
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+	var controller = new Controller
 }
-
 app()
+
---
+++ src/scripts/view/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var Header = React.createClass({
+	render: function(){
+		return(
+			<header>
+				instaclone
+			</header>
+		)
+	}
+})
+export default Header
